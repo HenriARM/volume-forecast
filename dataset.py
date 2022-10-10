074f4737eb1cbd5a370e2c6a60901b3863870a32
HenriARM <henriarm@yandex.ru>
2022-10-10 05:24:29 +0300

    Added dataset filling missed data + TA-lib features

--- dataset.py
+++ dataset.py
@@ -29,21 +29,16 @@ def process(chunk, extra, test_split):
     # merge Volumes of trades from same second
     train_df = chunk[['T', 'vol']].copy().groupby('T').sum().reset_index()
 
-    # check that datetime is in chronological order and equidisant (constant time intervals)
-    # chron_date = pd.date_range(start=train_df['T'].iloc[0], end=train_df['T'].iloc[-1], freq='1S')
-    # # append zero rows
-    # train_df = train_df.append([pd.Series(0, index=train_df.columns) for _ in range(len(chron_date) - len(train_df))],
-    #                            ignore_index=True)
-    # TODO: fill missing dates (pay attention that append changes type of 'T' from datetime to object )
-    # train_df['col'] = chron_date
+    # fill missing data with previous val
+    # make sure  datetime is in chronological order and equidisant (constant time intervals)
+    date_index = pd.date_range(start=train_df['T'].iloc[0], end=train_df['T'].iloc[-1], freq='S')
+    train_df = train_df.set_index('T').reindex(date_index, method='pad').reset_index().rename(columns={'index': 'T'})
 
     # cumulative sum of last 10s Volume
     train_df['vol_last_10'] = train_df['vol'].rolling(min_periods=1, window=10).sum().reset_index()['vol'].astype(
         np.float32)
     # shift to get Volume for next 10s
     train_df['vol_next_10'] = train_df['vol_last_10'].shift(-10)
-    # drop first 9 rows (don't know total Volume of first 9 seconds)
-    train_df = train_df.drop(list(range(9)))
     # create Target - Volume movement: (new price - old price) / old price
     train_df['vol_mov'] = (train_df['vol_next_10'] - train_df['vol_last_10']) / train_df['vol_last_10']
 
@@ -59,12 +54,16 @@ def process(chunk, extra, test_split):
         train_df['hour_sin'] = train_df['hour'].apply(lambda x: np.sin(np.pi * x / 12))
         train_df['hour_cos'] = train_df['hour'].apply(lambda x: np.cos(np.pi * x / 12))
 
-        train_df['MA'] = ta.SMA(df[['vol_last_10']], timeperiod=5)
-        train_df['EMA'] = ta.EMA(df['vol_last_10'], timeperiod=5)
+        timeperiod = 10
+        train_df['vol_last_10_MA'] = ta.SMA(train_df['vol_last_10'], timeperiod=timeperiod)
+        train_df['vol_last_10_EMA'] = ta.EMA(train_df['vol_last_10'], timeperiod=timeperiod)
+        train_df['vol_last_10_MACD'] = ta.MACD(train_df['vol_last_10'])[0]
+        # drop first timeperiod rows (don't know EMA and SMA)
+        train_df = train_df.drop(list(range(max(33, timeperiod - 1))))
         # ---- TODO: add more features here
-
-        # save processed dataframe to csv
-        # train_df[['T', 'vol_last_10', 'vol_next_10', 'vol_mov']].to_csv('./train_extra.csv')
+    else:
+        # drop first 9 rows (don't know last 10s Total Volume)
+        train_df = train_df.drop(list(range(9)))
 
     # normalize numerical columns
     cols_to_norm = ['vol_last_10']
@@ -91,7 +90,6 @@ def generate_dataset(filename, chunksize, extra, test_split):
             row = re.sub('\d+\.\d+\|', '', row)
             rows.append(row)
             i += 1
-            # # TODO:tmp rows to use from dataset
             # if i == TMPI:
             #     break
 
@@ -104,4 +102,4 @@ def generate_dataset(filename, chunksize, extra, test_split):
 
 
 if __name__ == '__main__':
-    generate_dataset(filename='./BTCUSDT_aggTrade.csv', chunksize=10000, extra=False, test_split=0)
+    generate_dataset(filename='./BTCUSDT_aggTrade.csv', chunksize=10000, extra=True, test_split=0)
