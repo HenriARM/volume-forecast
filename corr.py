a6802248e990b30834ef2653959702d6c6e67ac0
HenriARM <henriarm@yandex.ru>
2022-10-17 05:04:32 +0300

    Correlation script

---
+++ corr.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.figure(figsize=(7, 7))
+filename = './datasets/data.pkl'
+df = pd.read_pickle(filename)
+df = df[['vol_last_10', 'market_spread', 'mid_price',
+         'vol_imbalance', 'market_depth', 'ask_cv', 'bid_cv']][:100]
+# calculate the correlation matrix
+corr = df.corr()
+# plot the heatmap
+sns.heatmap(corr, xticklabels=corr.columns, yticklabels=corr.columns)
+plt.show()
+plt.savefig('./plots/corr.png')
