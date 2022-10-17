e35bb0084eb6c33a63f4eb2f0e959ccd2fc3861f
HenriARM <henriarm@yandex.ru>
2022-10-17 04:13:21 +0300

    Added ACF plot script

---
+++ acf.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import os
+
+output = './plots'
+features = ['vol_last_10', 'market_spread', 'mid_price',
+            'vol_imbalance', 'market_depth', 'ask_cv', 'bid_cv']
+
+filename = './datasets/data.pkl'
+data = pd.read_pickle(filename)
+for feature in features:
+    sm.graphics.tsa.plot_acf(
+        x=data['vol_last_10'],
+        lags=50,
+        title=f'{feature} ACF'
+    )
+    plt.savefig(os.path.join(output, f'acf_{feature}.png'))
+    plt.show()
